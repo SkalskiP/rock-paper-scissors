54eb353126241e3f6ea86386712abf40139f514c
SkalskiP <piotr.skalski92@gmail.com>
2022-11-15 15:32:50 +0100

    semi-working-version

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import './App.scss';
-import {GameView} from "./view/game-view/GameView";
+
+interface ISize {
+    width: number;
+    height: number;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <GameView/>
-    </div>
-  );
+    const appRef = useRef<HTMLDivElement>(null);
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const photoRef = useRef<HTMLCanvasElement>(null);
+
+    const [appSize, setAppSize] = useState<ISize>({width: 0, height: 0})
+
+    const getVideo = () => {
+        console.log('getVideo', 'appSize', appSize)
+        if (appSize.width === 0 || appSize.height === 0) return;
+
+        window.navigator.mediaDevices.getUserMedia({
+            video: {width: 1920, height: 1080}
+        }).then((stream: MediaStream) => {
+            if (videoRef.current) {
+                let video = videoRef.current;
+                video.srcObject = stream;
+                video.play()
+            }
+        }).catch(error => {
+            console.error(error)
+        })
+    }
+
+    useEffect(() => {
+        getVideo()
+    }, [videoRef, appSize])
+
+    useEffect(() => {
+        if (!appRef.current) return;
+
+        const newAppSize = {
+            width: appRef.current.clientWidth,
+            height: appRef.current.clientHeight
+        }
+
+        if (appSize.width !== newAppSize.width && appSize.height !== newAppSize.height) {
+            // todo: remove that console log
+            console.log('appSize', appSize, 'newAppSize', newAppSize, appSize !== newAppSize)
+            setAppSize(newAppSize)
+        }
+    }, [appRef, appSize])
+
+    return (
+        <div className="App" ref={appRef}>
+            <video ref={videoRef}></video>
+            {/*<div className="camera">*/}
+            {/*    */}
+            {/*</div>*/}
+            {/*<div className={"result" + hasPhoto ? "hasPhoto" : ""}>*/}
+            {/*    <canvas ref={photoRef}></canvas>*/}
+            {/*    <button>CLOSE</button>*/}
+            {/*</div>*/}
+        </div>
+    );
 }
 
 export default App;
