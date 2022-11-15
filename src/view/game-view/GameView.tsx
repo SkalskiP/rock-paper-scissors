b1fa76601e4d807f073f7ffa2d8baf0595774232
SkalskiP <piotr.skalski92@gmail.com>
2022-11-15 10:13:28 +0100

    commit all current changes

--- src/view/game-view/GameView.tsx
+++ src/view/game-view/GameView.tsx
@@ -1,24 +1,45 @@
-import React, {useRef} from 'react';
+import React, {useState} from 'react';
 import './GameView.scss';
+import Webcam from "react-webcam";
 
 export const GameView: React.FC = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [width, setWidth] = useState<number | null>(null);
+    const [height, setHeight] = useState<number | null>(null);
+
+    // const canvasRef = useRef<HTMLCanvasElement>(null);
 
     const onWindowResize = () => {
         console.log('onResize')
 
-        if (canvasRef.current) {
-            canvasRef.current.width = window.innerWidth
-            canvasRef.current.height = window.innerHeight
+        setHeight(window.innerHeight)
+        setWidth(window.innerWidth)
 
-            console.log("width", window.innerWidth)
-            console.log("height", window.innerHeight)
-        }
+        // if (canvasRef.current) {
+        //     canvasRef.current.width = window.innerWidth
+        //     canvasRef.current.height = window.innerHeight
+        //
+        //     console.log("width", window.innerWidth)
+        //     console.log("height", window.innerHeight)
+        // }
     }
 
     window.addEventListener("resize", onWindowResize)
 
+    const videoConstraints = {
+        width: width as number,
+        height: height as number,
+        facingMode: "user"
+    };
+
     return(<div className={"game-view"}>
-        <canvas ref={canvasRef}/>
+        {width && height && <Webcam
+            audio={false}
+            height={height}
+            // ref={webcamRef}
+            // screenshotFormat="image/jpeg"
+            width={width}
+            videoConstraints={videoConstraints}
+        />}
+        {/*<canvas ref={canvasRef}/>*/}
     </div>)
 }
